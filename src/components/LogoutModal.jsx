b48da21d20ec639c2831c3858be0cab4cfdfe790
PaulMarv <mpsalako@gmail.com>
2023-04-20 15:21:15 +0100

    handled logout modal

---
+++ src/components/LogoutModal.jsx
@@ -0,0 +1,27 @@
+import { useStateContext } from "../context/contextProvider"
+import{BsFillPatchExclamationFill} from 'react-icons/bs';
+import { useNavigate } from "react-router-dom";
+
+
+export const LogoutModal = ()=> {
+    const { logoutModal, setLogoutModal, setUser } = useStateContext();
+    const navigate = useNavigate()
+
+    const handleLogout = ()=>{
+        setLogoutModal(false)
+        setUser(null)
+        navigate('/')
+
+    }
+
+    if (!logoutModal) return null
+    return(
+        <div id='logoutContainer' onClick={(e)=> e.target.id==='logoutContainer'? setLogoutModal(false):null} className="inset-0 flex items-center justify-center fixed bg-black z-100 bg-opacity-30">
+            <div className="md:p-20 p-7 text-xs md:text-base flex flex-col justify-center items-center gap-y-2 bg-white text-white rounded-xl md:rounded-3xl md:h-2/4 h-[33.3%] aspect-square">
+                <div className="text-gray-700 md:text-red-500 text-5xl md:text-8xl"><BsFillPatchExclamationFill/></div>
+                <div className="md:mt-4 mt-2 text-blue text-center">Are you sure you want to log out of your account?</div>
+                <div><button onClick={handleLogout} className="md:mt-2 bg-blue rounded-full text-xxs md:text-xs md:w-20 md:h-9 w-14 h-6">Logout</button></div>
+            </div>
+        </div>
+    )
+}
--- src/components/UserProfileModal.jsx
+++ src/components/UserProfileModal.jsx
@@ -1,17 +1,90 @@
-import { useStateContext } from "../context/contextProvider"
+import { useStateContext } from "../context/contextProvider";
+import { HiUser } from "react-icons/hi2";
+import { BsArrowRight } from "react-icons/bs";
 
-export const UserProfileModal= ()=>{
-    const {profileModal, setProfileModal} = useStateContext()
-    const handleClose = (e) => {
-        if(e.target.id === "container") setProfileModal(false)
-    }
+export const UserProfileModal = () => {
+  const { profileModal, setProfileModal } = useStateContext();
+  const handleClose = (e) => {
+    if (e.target.id === "container") setProfileModal(false);
+  };
 
+  return (
+    <>
+      {profileModal ? (
+        <div
+          onClick={handleClose}
+          id="container"
+          className="inset-0 fixed bg-black z-50 bg-opacity-30"
+        />
+      ) : null}
 
-    return (<>
-        {profileModal ? <div onClick={handleClose} id='container' className="inset-0 fixed bg-black z-50 bg-opacity-30"/>
-        : null}
-    
-            <div className={`z-100 ${profileModal===false ?'translate-y-full md:translate-y-0 md:hidden':'translate-y-0'} ease-in-out duration-500 fixed h-[70%] w-full md:h-[85%] bottom-0 md:bottom-[7.5%] md:left-[30%]  md:w-[45%] rounded-t-2xl md:rounded-3xl bg-white`}>
+      <div
+        className={`z-100 ${
+          profileModal === false
+            ? "translate-y-full md:translate-y-0 md:hidden"
+            : "translate-y-0"
+        } ease-in-out duration-300 fixed h-[70%] w-full md:h-[92%] bottom-0 md:bottom-[4%] md:left-[30%]  md:w-[45%] rounded-t-2xl md:rounded-3xl bg-white`}
+      >
+        <div className="text-xxs md:text-xs px-6 md:px-16 pt-6 md:pt-12 w-full text-blue flex flex-col">
+          <div className=" text-2xl md:text-5xl mx-auto w-16 h-16 md:w-24 md:h-24 rounded-full ring-4 md:ring-6 ring-gray-400 flex items-center justify-center">
+            <HiUser />
+          </div>
+          <div className="text-center mt-6 text-base md:text-xl font-medium ">Adekunle Simeon</div>
+          <div className="text-center mt-1">University of Lagos</div>
+          <div className="text-center mt-1">Computer Science</div>
+          <div className="flex flex-col gap-y-4 mt-4">
+            <div>
+              <span className="text-xs md:text-sm font-medium">About</span>
+              <br />
+              <div className="md:leading-6">
+                Adekunle is a highly motivated computer science student with a
+                passion for coding and problem-solving. He is always eager to
+                learn new programming languages and improve his skills.
+              </div>
             </div>
-            </>)
-}
+            <div className="flex justify-between">
+              <div>
+                <span className="text-xs md:text-sm font-medium">Religion</span>
+                <br />
+                <span>Christian</span>
+              </div>
+              <div>
+                <span className="text-xs md:text-sm font-medium">Side Hustle</span>
+                <br />
+                <span>Freelance Web Developer</span>
+                <br />
+              </div>
+            </div>
+            <div>
+              <span className="text-xs md:text-sm font-medium">Schedule/Routine</span>
+              <br />
+              <span className="md:leading-6">
+                Adekunle wakes up at 6am every day, attends classes from 8am to
+                3pm, works on his freelance projects from 4pm to 7pm, plays
+                football in the evening and reads before going to bed.
+              </span>
+            </div>
+            <div>
+              <span className="text-xs md:text-sm font-medium">Hobbies</span>
+              <br />
+              <div className=" pl-2">
+                <li>Football</li>
+                <li>Reading</li>
+                <li>Coding</li>
+              </div>
+            </div>
+            <div
+              span
+              className="text-gray-500 font-medium text-center flex items-center justify-center"
+            >
+              <span>Edit Profile</span>{" "}
+              <>
+                <BsArrowRight />
+              </>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
