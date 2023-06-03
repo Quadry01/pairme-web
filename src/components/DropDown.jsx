4be4f89ad278472231108e461e122e08b40743e1
PaulMarv <mpsalako@gmail.com>
2023-06-03 17:57:16 +0100

    corrected dropdown

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -1,11 +1,12 @@
 import { BsChevronDown } from "react-icons/bs";
+import  './../App.css';
 
 export const DropDownComponent = ({
   pHolder
 }) => {
   return (
     <div className="relative h-full flex items-center text-[7.3px] md:text-[20px]">
-      <input className="border md:rounded-[9px] rounded-[9px] h-full border-blue bg-white md:px-3 px-1 w-[80px] md:w-[170px]" placeholder={`${pHolder}`}></input>
+      <input className="border md:rounded-[9px] rounded-[7px] h-full border-blue bg-white md:px-3 px-1 w-[80px] md:w-[170px] xs-dropDown" placeholder={`${pHolder}`}></input>
        <span className="absolute md:top-[12px] bottom-[7px] md:bottom-0 md:right-5 right-2 text-blue"><BsChevronDown/></span>
        {/* <div className="absolute md:bottom-[-110px] bg-white h-20 md:w-[300px] rounded-[12px] z-[65]">
        </div> */}
